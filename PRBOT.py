8cca9733d5b1b1ba51dac51d6573ad7ff2764748
Diego-bots-78 <papeleramadero@gmail.com>
2025-04-30 18:39:05 -0300

    Create PRBOT.py
    Bot trading

---
+++ PRBOT.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+import os
+
+TOKEN = os.getenv('TOKEN')
+
+def start(update, context):
+    update.message.reply_text("¡Bot activo desde la nube! ⚡")
+
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+print("Bot en línea...")
